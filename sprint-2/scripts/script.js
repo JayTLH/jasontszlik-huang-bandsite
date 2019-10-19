bbf9e4b5594b42c5c029ac20b2b46fc13420238a
jason tlh <jasonjdh94@gmail.com>
2019-10-19 13:55:07 -0400

    completed comment section functionality

--- sprint-2/scripts/script.js
+++ sprint-2/scripts/script.js
@@ -1,5 +1,5 @@
-// function to add comments into html using DOM
-function displayComment(image, name, date, comment) {
+// function to display comments into html using DOM
+function displayComment(object) {
     const commentList = document.querySelector('.comments__list');
 
     let commentListItem = document.createElement('li');
@@ -7,7 +7,7 @@ function displayComment(image, name, date, comment) {
     
     let commentUserImage = document.createElement('img');
     commentUserImage.classList.add('comments__user-img-past');
-    commentUserImage.setAttribute('src', image);
+    commentUserImage.setAttribute('src', object.image);
     commentUserImage.setAttribute('alt', 'profile picture of past commenter');
     
     let commentHeader = document.createElement('header');
@@ -15,19 +15,19 @@ function displayComment(image, name, date, comment) {
     
     let commentName = document.createElement('p');
     commentName.classList.add('comments__user-name-past');
-    commentName.textContent = name;
+    commentName.textContent = object.name;
     
     let commentDate = document.createElement('p');
     commentDate.classList.add('comments__date');
     commentDate.classList.add('font-label');
-    commentDate.textContent = date;
+    commentDate.textContent = object.date;
 
     let commentArticle = document.createElement('article');
     commentArticle.classList.add('comments__comment-past');
     
     let commentContent = document.createElement('p');
     commentContent.classList.add('comments__comment-content');
-    commentContent.textContent = comment;
+    commentContent.textContent = object.comment;
     
     commentList.appendChild(commentListItem);
     commentListItem.appendChild(commentUserImage);
@@ -38,16 +38,18 @@ function displayComment(image, name, date, comment) {
     commentArticle.appendChild(commentContent);
 }
 
-// function to add info into an object and unshift it into an array
+// function to add a new comment as an object and unshift it into an array
 function addNewComment() {
     const commentForm = document.querySelector('.comments__form')
     let newCommenter = {};
+    let today = new Date();
+    let date = (today.getMonth()+1) + '/' + today.getDate() + '/' + today.getFullYear();
     commentForm.addEventListener('submit', (event) => {
         event.preventDefault();
         newCommenter = {
             image: '../assets/images/Mohan-muruge.jpg',
             name: event.target['user-name'].value,
-            date: '10/18/2019',
+            date: date,
             comment: event.target['new-comment'].value
         };
         commenters.unshift(newCommenter);
@@ -58,41 +60,39 @@ function addNewComment() {
         createCommentList.classList.add('comments__list');
         commentSection.appendChild(createCommentList);
         commenters.forEach(function(index) {
-            displayComment(index.image, index.name, index.date, index.comment);
+            displayComment(index);
+        event.target['user-name'].value = '';
+        event.target['new-comment'].value = '';
         })
     })
     return newCommenter
 }
 
-// function removeComments() {
-//     let noList = document.querySelector('');
-// }
-
 const commenters = [
     {
         image: '../assets/images/no-profile-pic.jpg',
-        name: 'Jason',
+        name: 'Wolf',
         date: '10/18/2019',
-        comment: 'Hey this is a comment3'
+        comment: "Hey, can't we all just get along? I mean music is suppose to bring people together. I agree it wasn't their best work but I had fun."
     },
 
     {
         image: '../assets/images/no-profile-pic.jpg',
         name: 'Kiel',
         date: '10/17/2019',
-        comment: 'Hey this is a comment2'
+        comment: 'I have no idea what the other guy was talking about, I mean this concert was hot garbage. Its such a shame to see the band I love fall from grace.'
     },
 
     {
         image: '../assets/images/no-profile-pic.jpg',
         name: 'Ignis',
         date: '10/16/2019',
-        comment: 'Hey this is a comment1'
+        comment: 'Yoooo! The concert was FIYA! By far the best event I ever attended, wish I could relieve the moment.'
     },
 ]
 
 commenters.forEach(function(index) {
-    displayComment(index.image, index.name, index.date, index.comment);
+    displayComment(index);
 })
 
 addNewComment();
